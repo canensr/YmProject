5d1310c0b8457be3153763d3d6c947cefbec683a
Ensar CAN <ensarcan57@gmail.com>
2022-11-10 23:15:54 +0300

    Ym Project
    Etkinlik, toplantı vb. planlayıcı

---
+++ DoodleProject/EntityLayer/Concrete/Activity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Activity
+    {
+        [Key]
+        public int ActivityID { get; set; }
+        [StringLength(50)]
+        public string ActivitySubject { get; set; }
+        [StringLength(200)]
+        public string ActivityExplanation { get; set; }
+        [StringLength(5)]
+        public string ActivityDuration { get; set; }
+        [StringLength(15)]
+        public string ActivityDate { get; set; }    
+    }
+}
