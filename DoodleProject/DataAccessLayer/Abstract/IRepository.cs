2f38d087239537e628c9d8ec1382ffd6d36ddaed
Ensar CAN <ensarcan57@gmail.com>
2022-11-10 23:12:03 +0300

    Ym Project

---
+++ DoodleProject/DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();
+        int Insert(T P);
+        int Update(T P);
+        int Delete(T P);
+        T GetByID(int id);
+
+        List<T> List(Expression<Func<T, bool>> filter);
+
+        T Find(Expression<Func<T, bool>> where);
+    }
+}
---
+++ DoodleProject/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext
+    {
+        public Context()
+        {
+            Database.Connection.ConnectionString = "Server=PC;Database=DoodleDb;integrated security=True";
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
